f3a4bca3df90cd341d0ed7b0a0752d4a2222dbaf
AV\ahmedabdellatif <ahmedabdellatif7700@gmail.com>
2024-08-27 17:12:52 +0100

    Data Types examples

--- button_LED/button_LED.srcs/sources_1/new/BUTTON_LED.vhd
+++ button_LED/button_LED.srcs/sources_1/new/BUTTON_LED.vhd
@@ -17,30 +17,47 @@
 -- Additional Comments:
 -- 
 ----------------------------------------------------------------------------------
+-- -- VHDL Data Type Example
+-- -- Standard Logic
+-- SIGNAL sig_A : STD_LOGIC := '0';
+-- SIGNAL sig_B : STD_LOGIC := '1';
+-- sig_A <= '1';
+-- sig_A <= '0';
+-- sig_A <= sig_B;
+-- -- Standard logic vector
+-- SIGNAL sig_C : STD_LOGIC_VECTOR(5 DOWNTO 0) := "000000";
+-- SIGNAL sig_D : STD_LOGIC_VECTOR(5 DOWNTO 0) := (OTHERS => '1'); -- that will make them all logic 1
+-- sig_C <= sig_D; --since both have the same width
+-- sig_C(3) <= sig_D(0);
+
+-- --unsigned data types 
+-- SIGNAL sig_E : UNSIGNED(3 DOWNTO 0) := (OTHERS => '1');
+-- SIGNAL sig_F : UNSIGNED(3 DOWNTO 0) := "0010"; --2
+-- sig_E <= (OTHERS => '0');
+-- sig_F <= sig_F + 1; -- also valid
+
+-- --signed data types
+-- SIGNAL sig_G : SIGNED(3 DOWNTO 0) := (OTHERS => '1');
+-- SIGNAL sig_H : SIGNED(3 DOWNTO 0) := "1011"; --5
+-- sig_G <= (OTHERS => '0');
+-- sig_F <= sig_G - 1; -- 
+----------------------------------------------------------------------------------
+
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
-ENTITY EXAMPLE IS
+ENTITY BUTTON_LED IS
     --  Port ( );
     GENERIC (
-        Bits : int := 3); -- we will use this generic bits to define inputs and outputs
+        NUM_BUTTONS : int := 4);
     PORT (
-        A : IN STD_LOGIC_VECTOR(Bits - 1 DOWNTO 0);
-        B : IN STD_LOGIC_VECTOR(Bits - 1 DOWNTO 0);
-        C : OUT STD_LOGIC;
+        LED_OUT : IN STD_LOGIC_VECTOR(Bits - 1 DOWNTO 0);
+        BUTTON_IN : IN STD_LOGIC_VECTOR(Bits - 1 DOWNTO 0);
+        ENABLE : IN STD_LOGIC;
     );
-END EXAMPLE;
+END BUTTON_LED;
 
 ARCHITECTURE BEHAVE OF BUTTON_LED IS
 BEGIN
-    C <= A AND B;
+
 END BEHAVE;
